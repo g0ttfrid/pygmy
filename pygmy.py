232b888366d6b06433dfa98c7a5ebaa874fb790b
Yuri M <yurimaia@protonmail.ch>
2022-08-11 15:40:17 -0300

    Update pygmy.py

--- pygmy.py
+++ pygmy.py
@@ -12,9 +12,10 @@ regex = {
 "aws_mws_key": r"amzn\.mws\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",
 "aws_secret_key": r"(?i)(secret|aws)(.{0,21})?['\"][0-9a-zA-Z\/+]{40}['\"]",
 "aws_bucket": r"s3\.amazonaws.com[/][A-Za-z0-9_-]+|[a-zA-Z0-9_-]*\.s3.amazonaws.com",
-"google_api_key": r"[A-Za-z0-9_]{21}--[A-Za-z0-9_]{8}",
+"google_api_key": r"AIza[0-9A-Za-z\\-_]{35}",
 "google_oauth_2.0": r"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}",
 "google_cloud_plataform": r"(?i)(google|gcp|youtube|drive|yt)(.{0,20})?['\"][AIza[0-9a-z\\-_]{35}]['\"]",
+"google_cloud_apikey": r"[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}",
 "slack_token": r"xox[baprs]-([0-9a-zA-Z]{10,48})?",
 "basic_auth": r"(?<=:\/\/)[a-zA-Z0-9]+:[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
 "github": r"(?i)github(.{0,20})?['\"][0-9a-zA-Z]{35,40}"}
